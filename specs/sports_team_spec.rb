322ebb29c8d1acafb8b273f0706cbd30108c3a67
Ross Lorimer <wross.lorimer@googlemail.com>
2019-09-16 21:37:45 +0100

    Part B completed

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -6,23 +6,40 @@ class TestSportsTeam < MiniTest::Test
 
   def test_get_team_name
     sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
-    assert_equal("Ross's Rovers", sports_team.get_team_name())
+    assert_equal("Ross's Rovers", sports_team.team_name())
   end
 
   def test_get_players
     sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
-    assert_equal(["Ross", "Rossy", "Rossario"], sports_team.get_players())
+    assert_equal(["Ross", "Rossy", "Rossario"], sports_team.players())
   end
 
   def test_get_team_coach
     sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
-    assert_equal("Billy Rossco", sports_team.get_team_coach())
+    assert_equal("Billy Rossco", sports_team.team_coach())
   end
 
   def test_set_team_coach
     sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
-    sports_team.set_team_coach("Rossco Lorimerio")
-    assert_equal("Rossco Lorimerio", sports_team.get_team_coach())
+    sports_team.team_coach = "Rossco Lorimerio"
+    assert_equal("Rossco Lorimerio", sports_team.team_coach)
   end
 
+  def test_add_player
+    sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
+    sports_team.add_player("William")
+    assert_equal(["Ross", "Rossy", "Rossario", "William"], sports_team.players)
+  end
+
+  def test_is_player_in_team
+    sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
+    sports_team.is_player_in_team("Rossy")
+    assert_equal("Rossy", sports_team.is_player_in_team("Rossy"))
+  end
+
+  def test_team_win__yes
+    sports_team = SportsTeam.new("Ross's Rovers", ["Ross", "Rossy", "Rossario"], "Billy Rossco")
+    sports_team.team_win("win")
+    assert_equal(3, sports_team.team_win("win"))
+  end
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,25 +1,29 @@
 class SportsTeam
 
+  attr_accessor :team_name, :players, :team_coach, :points
 
   def initialize(input_team_name, input_players, input_team_coach)
     @team_name = input_team_name
     @players = input_players
     @team_coach = input_team_coach
+    @points = 0
   end
 
-  def get_team_name
-    return @team_name
+  def add_player(name)
+    return @players.push(name)
   end
 
-  def get_players
-    return @players
+  def is_player_in_team(name)
+    player = []
+    for player in @players
+      return player if player == name
+    end
   end
 
-  def get_team_coach
-    return @team_coach
-  end
-
-  def set_team_coach(new_coach)
-    @team_coach = new_coach
+  def team_win(result)
+    @points = 0
+    if result == "win"
+      @points +=3
+    end
   end
 end
